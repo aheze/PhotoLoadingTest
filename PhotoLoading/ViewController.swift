5b57caea831c09654b58b7b00571cd804edae77b
Zheng <aheze@getfind.app>
2022-09-30 13:49:58 -0700

    Initial Commit

---
+++ PhotoLoading/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PhotoLoading
+//
+//  Created by A. Zheng (github.com/aheze) on 9/30/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
