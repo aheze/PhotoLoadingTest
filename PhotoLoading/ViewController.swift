556a3d475913795561de090778a02a22077c80e7
Zheng <aheze@getfind.app>
2022-10-02 17:51:54 -0700

    Add comments

--- PhotoLoading/ViewController.swift
+++ PhotoLoading/ViewController.swift
@@ -10,13 +10,22 @@ import Photos
 import SwiftUI
 
 class ViewController: UIViewController {
+    let imageManager = PHCachingImageManager()
+    let thumbnailSize = CGSize(width: 30, height: 30)
+
+    var previousPreheatRect = CGRect.zero
     var fetchResult: PHFetchResult<PHAsset>?
 
-    var layout: UICollectionViewFlowLayout = {
+    var options: PHImageRequestOptions = {
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .opportunistic
+        return options
+    }()
+
+    lazy var layout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-//        layout.itemSize = CGSize(width: 100, height: 100)
-        layout.itemSize = CGSize(width: 30, height: 30) /// lower lengths = more cells
+        layout.itemSize = thumbnailSize
         return layout
     }()
 
@@ -28,7 +37,10 @@ class ViewController: UIViewController {
         view.addSubview(collectionView)
         collectionView.pinEdgesToSuperview()
         collectionView.dataSource = self
+        collectionView.delegate = self
         collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+
+        // MARK: - Get assets
 
         switch PHPhotoLibrary.authorizationStatus(for: .readWrite) {
         case .limited, .authorized:
@@ -51,6 +63,8 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - Data source
+
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return fetchResult?.count ?? 0
@@ -60,31 +74,102 @@ extension ViewController: UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
 
         if let fetchResult {
-            let asset = fetchResult.object(at: indexPath.item)
-            let options = PHImageRequestOptions()
-            options.deliveryMode = .opportunistic
-            let length = cell.bounds.width * UIScreen.main.scale
-
+            /// Cancel the request if the cell was reused.
             if let imageRequestID = cell.imageRequestID {
-                PHImageManager.default().cancelImageRequest(imageRequestID)
+                imageManager.cancelImageRequest(imageRequestID)
                 cell.imageRequestID = nil
             }
 
-            DispatchQueue.global(qos: .userInitiated).async {
-                let imageRequestID = PHImageManager.default().requestImage(
-                    for: asset,
-                    targetSize: .init(width: length, height: length),
-                    contentMode: .aspectFill,
-                    options: nil
-                ) { image, _ in
-                    DispatchQueue.main.async {
-                        cell.imageView.image = image
-                    }
-                }
-                cell.imageRequestID = imageRequestID
+            let asset = fetchResult.object(at: indexPath.item)
+
+            /// Request the image. This is where the lag happens - if you delete this, scrolling will be smooth again.
+            let imageRequestID = imageManager.requestImage(
+                for: asset,
+                targetSize: thumbnailSize,
+                contentMode: .aspectFill,
+                options: nil
+            ) { image, _ in
+
+                cell.imageView.image = image
             }
+            cell.imageRequestID = imageRequestID
         }
         return cell
+    }
+}
+
+// MARK: - Delegate and asset caching
+
+/// from https://developer.apple.com/documentation/photokit/browsing_and_modifying_photo_albums
+extension ViewController: UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateCachedAssets()
+    }
+
+    fileprivate func resetCachedAssets() {
+        imageManager.stopCachingImagesForAllAssets()
+        previousPreheatRect = .zero
+    }
+
+    /// - Tag: UpdateAssets
+    fileprivate func updateCachedAssets() {
+        // Update only if the view is visible.
+        guard isViewLoaded, view.window != nil else { return }
+
+        // The window you prepare ahead of time is twice the height of the visible rect.
+        let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.bounds.size)
+        let preheatRect = visibleRect.insetBy(dx: 0, dy: -0.5 * visibleRect.height)
+
+        // Update only if the visible area is significantly different from the last preheated area.
+        let delta = abs(preheatRect.midY - previousPreheatRect.midY)
+        guard delta > view.bounds.height / 3 else { return }
+
+        // Compute the assets to start and stop caching.
+        let (addedRects, removedRects) = differencesBetweenRects(previousPreheatRect, preheatRect)
+        let addedAssets = addedRects
+            .flatMap { rect in collectionView.indexPathsForElements(in: rect) }
+            .map { indexPath in fetchResult!.object(at: indexPath.item) }
+        let removedAssets = removedRects
+            .flatMap { rect in collectionView.indexPathsForElements(in: rect) }
+            .map { indexPath in fetchResult!.object(at: indexPath.item) }
+
+        // Update the assets the PHCachingImageManager is caching.
+        imageManager.startCachingImages(
+            for: addedAssets,
+            targetSize: thumbnailSize, contentMode: .aspectFill, options: nil
+        )
+        imageManager.stopCachingImages(
+            for: removedAssets,
+            targetSize: thumbnailSize, contentMode: .aspectFill, options: nil
+        )
+        // Store the computed rectangle for future comparison.
+        previousPreheatRect = preheatRect
+    }
+
+    fileprivate func differencesBetweenRects(_ old: CGRect, _ new: CGRect) -> (added: [CGRect], removed: [CGRect]) {
+        if old.intersects(new) {
+            var added = [CGRect]()
+            if new.maxY > old.maxY {
+                added += [CGRect(x: new.origin.x, y: old.maxY,
+                                 width: new.width, height: new.maxY - old.maxY)]
+            }
+            if old.minY > new.minY {
+                added += [CGRect(x: new.origin.x, y: new.minY,
+                                 width: new.width, height: old.minY - new.minY)]
+            }
+            var removed = [CGRect]()
+            if new.maxY < old.maxY {
+                removed += [CGRect(x: new.origin.x, y: new.maxY,
+                                   width: new.width, height: old.maxY - new.maxY)]
+            }
+            if old.minY < new.minY {
+                removed += [CGRect(x: new.origin.x, y: old.minY,
+                                   width: new.width, height: new.minY - old.minY)]
+            }
+            return (added, removed)
+        } else {
+            return ([new], [old])
+        }
     }
 }
 
@@ -115,5 +200,12 @@ extension UIView {
             bottomAnchor.constraint(equalTo: superview.bottomAnchor),
             leftAnchor.constraint(equalTo: superview.leftAnchor)
         ])
+    }
+}
+
+private extension UICollectionView {
+    func indexPathsForElements(in rect: CGRect) -> [IndexPath] {
+        let allLayoutAttributes = collectionViewLayout.layoutAttributesForElements(in: rect)!
+        return allLayoutAttributes.map { $0.indexPath }
     }
 }
